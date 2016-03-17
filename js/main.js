05939746365c403504f26c8712a5f7bdde996f48
HeatherAnn <heather.bisaillion@gmail.com>
2016-03-17 12:09:48 -0400

    Adding headings and organizing lists.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var $h1Above = $('<h1>');
+var $h1Below = $('<h1>');
+var $liAbove = $('<ul>');
+var $liBelow = $('<ul>');
+
+$h1Above.html('Above ground veggies');
+$h1Below.html('Below ground veggies');
+
+$('body').append($h1Above);
+$('body').append($ulAbove);
+
+$('body').append($h1Below);
+$('body').append($ulBelow);
+
+$('ul li').each(function() {
+  if($(this).hasClass('above')){
+    $ulAbove.append($(this))
+  } else {
+    $ulBelow.append($(this))
+  }
+});
